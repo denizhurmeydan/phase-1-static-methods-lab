09437de1af249df924e5816705ec50a634f6acd1
Deniz Hürmeydan <denizhurmeydan@gmail.com>
2023-08-15 00:32:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+ 
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^\w\s-']/g, '');
+  }
+
+  static titleize(str) {
+    const lowercaseWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = str.split(' ');
+
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !lowercaseWords.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    });
+
+    return titleizedWords.join(' ');
+  }
+
 }
